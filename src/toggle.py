4f887c9b0d7cfba93c43a5db0a6a7f5d0545e601
kanekotic <me@kanekotic.com>
2020-08-15 11:26:17 +0200

    rename main file

---
+++ src/toggle.py
@@ -0,0 +1,5 @@
+class Togglee:
+    def __init__(self, url: str, refresh_rate: int, defaults: dict):
+        self.url = url
+        self.refresh_rate = refresh_rate
+        self.defaults = defaults
