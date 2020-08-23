6d20523bbf7ac976b0da3a67d517063257cbbda7
kanekotic <me@kanekotic.com>
2020-08-23 14:42:31 +0200

    fix installation file

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-
+import os
 from setuptools import setup, find_packages
-
 
 with open('README.rst') as f:
     readme = f.read()
@@ -8,14 +7,18 @@ with open('README.rst') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+with open('requirements_prod.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='togglee',
-    version='0.0.1',
+    version=os.getenv('GITHUB_RUN_NUMBER', '0.0.1-SPANPSHOT'),
     description='Simple toggles for python from a url reference',
     long_description=readme,
     author='kanekotic',
     author_email='me@kanekotic.com',
     url='https://github.com/togglee/togglee-python',
+    install_requires=required,
     license=license,
-    packages=find_packages(where="src")
+    packages=find_packages()
 )
